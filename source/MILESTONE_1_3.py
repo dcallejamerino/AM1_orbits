786588a4ad229aeffab340d84765d918a8eef086
DAVID CALLEJA MERINO <dcalleja@alumnos.upm.es>
2023-09-28 13:36:31 -0400

    New RK4

--- source/MILESTONE_1_3.py
+++ source/MILESTONE_1_3.py
@@ -1,32 +1,48 @@
-# 3. Write a script to integrate Kepler Orbits with an Runge-Kutta 4th Order Method
-from numpy import array, zeros, linspace
-import matplotlib.pyplot as plt
+# MILESTONE 1 Write a script to integrate Kepler Orbits with Euler, Crank-Nicolson and RK4 Method
 
+from numpy import array, zeros, linspace
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+import numpy as np
+    
 U = array( [ 1, 0, 0, 1 ])
     
-N = 100000 
-dt = 0.0001
+N = 10000
 x = array( zeros(N) )
 y = array( zeros(N) )
+t = array( zeros(N) )
+
 x[0] = U[0] 
 y[0] = U[1]
-def F_Kepler(U):
-   x , y , vx , vy = U[0] , U[1] , U[2] , U[3]
-   mr = (x**2 + y**2)**1.5
-   return array ( [vx , vy , -x/mr , -y/mr] )
+t[0] = 0 
     
+def Kepler(U, t): 
+
+    x = U[0]; y = U[1]; dxdt = U[2]; dydt = U[3]
+    d = ( x**2  +y**2 )**1.5
+
+    return  array( [ dxdt, dydt, -x/d, -y/d ] ) 
+
+
+def RK4(U, dt, t, F ): 
+
+     k1 = F( U, t)
+     k2 = F( U + dt * k1/2, t + dt/2 )
+     k3 = F( U + dt * k2/2, t + dt/2 )
+     k4 = F( U + dt * k3,   t + dt   )
+ 
+     return  U + dt * ( k1 + 2*k2 + 2*k3 + k4 )/6
+
 for i in range(1, N): 
-    F = F_Kepler(U)
-    Y1 = F
-    Y2 = F+0.5*dt*Y1
-    Y3 = F+0.5*dt*Y2
-    Y4 = F+dt*Y3
-    U = U + dt/6*(Y1+2*Y2+2*Y3+Y4) 
-    x[i] = U[0] 
-    y[i] = U[1]
-    
+
+      dt = 0.01 
+      t[i] = dt*i
+      U = RK4 (U, dt, t, Kepler)
+      x[i] = U[0] 
+      y[i] = U[1]
+
 plt.plot(x, y)
 plt.xlabel('X Position')
 plt.ylabel('Y Position')
-plt.title('Kepler Orbit (RUNGE-KUTTA 4 METHOD)')
+plt.title('Kepler Orbit (RK4 METHOD)')
 plt.show()  
