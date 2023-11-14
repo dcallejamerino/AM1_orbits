b25da97ae86392e73384d8660e5d9040862df303
DAVID CALLEJA <dcallejamerino@gmail.com>
2023-11-14 05:46:24 -0500

    problemas martes 14 Nov

--- source/Physics.py
+++ source/Physics.py
@@ -43,3 +43,31 @@ def Clase3(U, t): #Duffing con k=1, B=1
     y = U[1]
     
     return  array ( [y, -x**3-y+cos(t)] )
+
+def Clase4(U, t): #Duffing con k=0.05, B=1 (a mayor k mas caotico)
+
+    x = U[0]
+    y = U[1]
+    
+    return  array ( [y, -x**3-0.05*y+cos(t)] )
+
+def Clase5(U, t): #Duffing con k=0.0001, B=1
+
+    x = U[0]
+    y = U[1]
+    
+    return  array ( [y, -x**3-0.001*y+cos(t)] )
+
+def Clase6(U, t): #Van der Pol con wo=1, u=0.2
+
+    x = U[0]
+    y = U[1]
+    
+    return  array ( [y, -1*x-0.2*(x**2-1)*y] )
+
+def Clase7(U, t): #Rayleigh fo=0.6 (periodico), f0=0.7 (ciclo doble: se repite cada 2 vueltas), fo=0.75 (ciclo cuadruple: se repite cada 4 vueltas)
+
+    x = U[0]
+    y = U[1]
+    
+    return  array ( [y, x-x**3-y+0.6*cos(t)] )
