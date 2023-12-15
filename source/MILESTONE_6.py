85c946ea5b66ccbc02ae13140c97a607801d2214
DAVID CALLEJA <dcallejamerino@gmail.com>
2023-12-15 03:21:52 -0500

    Cosmetic M6

--- source/MILESTONE_6.py
+++ source/MILESTONE_6.py
@@ -8,7 +8,13 @@ from Physic.Nbody_function import N_Body
 from Physic.Resolution_3Body import CR3BP, Lpoints, Stability
 from Stability.Stability_Regions import Stability_region
 
-### CAUCHY ###
+
+### MAIN INPUTS FOR THE CALCULATION ###
+sel_LG = 5  # which Lagrange Point we desire to simulate in the last graphs of the script (LP5 by default)
+orbitcalculation = 1 # will allow to calculate a longer or shorter orbit around a Lagrange Point (values between 1 and 10 presents good results at low computation times)
+
+
+## CAUCHY ##
 # Cauchy for Lagrange problem 
 
 def Cauchy_problem_Lagrange(F, t, U0, temporal_scheme): # rows/columns inverted versus the standard cauchy
@@ -23,31 +29,33 @@ def Cauchy_problem_Lagrange(F, t, U0, temporal_scheme): # rows/columns inverted 
     return U
 
 
-# Final time and number of divisions
+##  Final time and number of divisions ## 
 
 N = int(1e4)
-t = linspace(0, 1, N)
+t = linspace(0, orbitcalculation, N)
 mu = 1.2151e-2 #Earth-Moon
 #mu = 3.0039e-7 #Sun-Moon
 
 
-# Cauchy function that I call since it has more inputs than (U,t)
+##  Cauchy function that I call since it has more inputs than (U,t) ## 
+
 def F(U,t):
    return CR3BP(U, mu)
 
 
-# Lagrage points starting from close points
+##  Lagrage points starting from close points ## 
+
 U0LP = array([[0.1, 0, 0, 0],[1.01, 0, 0, 0],[-0.1, 0, 0, 0],[0.8, 0.6, 0, 0],[0.8, -0.6, 0, 0]])
 LagPoints = Lpoints(U0LP, 5, mu)
 
-# Print the calculated Lagrange points with labels
+##  Print the calculated Lagrange points with labels in th command window ## 
+
 for i, lag_point in enumerate(LagPoints):
     label = f"L{i + 1}"  # Adjust the index based on your preferred numbering (e.g., L1, L2, etc.)
     print(f"{label}: {lag_point}")
 
 
-# Generation of initial condicions close to a Lagrange point
-sel_LG = 5
+##  Generation of initial condicions close to a Lagrange point ## 
 
 U0 = zeros(4)
 U0[0:2] = LagPoints[sel_LG-1,:]
@@ -55,12 +63,14 @@ ran = 1e-4*random()
 U0 = U0 + ran
 
 
-#Integration of the circular restricted problem of the 3 Bodies thru a temporal scheme
+## Integration of the circular restricted problem of the 3 Bodies thru a temporal scheme  ## 
+
 temporal_scheme = CN
 U  = Cauchy_problem_Lagrange(F, t, U0, temporal_scheme)
 
 
-#Evaluation of the stability in Lagrange points
+ ## Evaluation of the stability in Lagrange points ## 
+
 for i in range(5):
     U0S = zeros(4)
     U0S[0:2] = LagPoints[i,:]
@@ -88,11 +98,11 @@ eingvalues5 = Stability(U0S, mu)
 # CN
 (x1, y1, rho1) = Stability_region(CN,100,0.0005,-0.0005,0.0005,-0.0005)
 plt.contour( x1, y1, transpose(rho1), linspace(0, 1, 11))
-plt.scatter(real(eingvalues1*1/N), imag(eingvalues1*1/N), color='red',label='LP1: dt=1/N')
-plt.scatter(real(eingvalues2*1/N), imag(eingvalues2*1/N), color='blue',label='LP2: dt=1/N')
-plt.scatter(real(eingvalues3*1/N), imag(eingvalues3*1/N), color='orange',label='LP3: dt=1/N')
-plt.scatter(real(eingvalues4*1/N), imag(eingvalues4*1/N), color='yellow',label='LP4: dt=1/N')
-plt.scatter(real(eingvalues5*1/N), imag(eingvalues5*1/N), color='green',label='LP5: dt=1/N')
+plt.scatter(real(eingvalues1*orbitcalculation/N), imag(eingvalues1*orbitcalculation/N), color='red',label='LP1: dt={}'.format(orbitcalculation/N))
+plt.scatter(real(eingvalues2*orbitcalculation/N), imag(eingvalues2*orbitcalculation/N), color='blue',label='LP2: dt={}'.format(orbitcalculation/N))
+plt.scatter(real(eingvalues3*orbitcalculation/N), imag(eingvalues3*orbitcalculation/N), color='orange',label='LP3: dt={}'.format(orbitcalculation/N))
+plt.scatter(real(eingvalues4*orbitcalculation/N), imag(eingvalues4*orbitcalculation/N), color='yellow',label='LP4: dt={}'.format(orbitcalculation/N))
+plt.scatter(real(eingvalues5*orbitcalculation/N), imag(eingvalues5*orbitcalculation/N), color='green',label='LP5: dt={}'.format(orbitcalculation/N))
 plt.legend()
 plt.xlabel('Re(w)')
 plt.ylabel('Im(w)') 
@@ -104,7 +114,7 @@ plt.show()
 
 
 
-#Graphs
+ ## Graphs  ## 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 ax1.plot(U[:,0], U[:,1],'-',color = "r")
 ax1.plot(-mu, 0, 'o', color = "g")
@@ -115,7 +125,7 @@ ax2.plot(U[:,0], U[:,1],'-',color = "r")
 ax2.plot(LagPoints[sel_LG-1,0], LagPoints[sel_LG-1,1] , 'o', color = "k")
 ax1.set_title("Orbital view")
 ax2.set_title("Close-up")
-fig.suptitle("Orbit around L5 with CN")
+fig.suptitle("Orbit around L{} with CN".format(sel_LG))
 for ax in fig.get_axes():
     ax.set(xlabel='x', ylabel='y')
     ax.grid()
